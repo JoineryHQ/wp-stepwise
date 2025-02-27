4c2f0d5a0021e1f50a4a619f5ac2af8101828b35
Allen Shaw <allen@JoineryHQ.com>
2025-02-27 15:09:25 -0600

    Clean up asset handling (move css, js assets to civicrm extension).

--- includes/class-shortcode.php
+++ includes/class-shortcode.php
@@ -9,16 +9,21 @@ class StepwiseShortcode {
    * Get array of plugin metadata.
    */
   public static function StepwiseButton($atts, $content, $shorcode_tag) {
-    if (!function_exists('civicrm_initialize')) {
-      // CiviCRM is not installed; print nothing.
+    if (!self::isCivicrmAndExtensionInitialize()) {
+      // If we don't have BOTH civicrm and the stepw extension, return empty.
       return '';
     }
-    
-    civicrm_initialize();
 
+    try {
     $ret = CRM_Stepw_Utils_WpShortcode::getStepwiseButtonHtml()
       . CRM_Stepw_Utils_WpShortcode::getProgressBarHtml();
-                  
+    }
+    catch (CRM_Stepw_Exception $e) {
+      // Although civicrm has been initialized, we're not actually IN civicrm now,
+      // so our hook_civicrm_unhandled_exception won't be invoked. Call it
+      // specifically here.
+      stepw_civicrm_unhandled_exception($e);
+    }
     // fixme3: if this is a video page, append video-validating JS.
 
     return $ret;
@@ -27,9 +32,47 @@ class StepwiseShortcode {
   public static function StepwiseButtonAssets() {
     global $post;
     if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'stepwise-button')) {
-      wp_enqueue_style('stepwise-button-css', STEPWISE_PLUGIN_URL . '/css/stepwise-button.css');
-      wp_enqueue_script('jquery');
-      wp_enqueue_script('stepwise-button-js', STEPWISE_PLUGIN_URL . '/js/stepwise-button.js');
+      if (!self::isCivicrmAndExtensionInitialize()) {
+        // If we don't have BOTH civicrm and the stepw extension, there's nothing to do here.
+        return;
+      }
+      $assets = CRM_Stepw_Utils_WpShortcode::getPageAssets();
+      foreach ($assets as $asset) {
+        $assetHandle = $asset['handle'];
+        $assetSrc = ($asset['src'] ?? '');
+        switch ($asset['type']) {
+          case 'style':
+            wp_enqueue_style($assetHandle, $assetSrc);
+            break;
+          case 'script':
+            wp_enqueue_script($assetHandle, $assetSrc);
+            break;
+        }
+      }
     }
+  }
+  
+  /**
+   * Initialize civicrm if possible, and tell whether both civicrm is activated AND
+   * the stepw extension is enabled in civicrm.
+   * 
+   * @return bool
+   */
+  private static function isCivicrmAndExtensionInitialize() {
+    static $ret;
+    if (!isset($ret)) {
+      $ret = FALSE;
+      if (function_exists('civicrm_initialize')) {
+        // CiviCRM is activated; initialize it.
+        civicrm_initialize();
+
+        $extMgr = CRM_Extension_System::singleton()->getManager();
+        if ($extMgr->isEnabled('com.joineryhq.stepw')) {
+          // stepw extension is enabled. That's everything we need.
+          $ret = TRUE;
+        }
+      }
+    }
+    return $ret;
   }
 }
--- js/stepwise-button.js
+++
@@ -1,9 +0,0 @@
-  /*
- */
-
-jQuery(function ($) {
-  // Move progress bar to just before h1 tag.
-  $('h1').before($('.stepwise-progress-wrapper'));
-  $('.stepwise-progress-wrapper').show();
-});
-
